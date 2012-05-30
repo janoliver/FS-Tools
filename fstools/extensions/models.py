70fe57c8c8c62424031cd9b80703494c90e974da
Jan Oliver Oelerich <janoliver@oelerich.org>
2012-05-30 08:35:01 +0200

    small changes

---
+++ fstools/extensions/models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from datetime import datetime
+
+
+class Timestamped_Model(models.Model):
+
+    date_created = models.DateTimeField(editable=False,
+            default=datetime.now)
+    date_modified = models.DateTimeField(editable=False,
+            default=datetime.now)
+
+
+    class Meta:
+
+        abstract = True
+
+
+    def save(self):
+        self.date_modified = datetime.now()
+        super(Timestamped_Model, self).save()
+
+
---
+++ fstools/extensions/templates.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from jinja2 import Environment
+from django.shortcuts import render_to_response as rtr
+import os
+import inspect
+from fstools.settings import root
+
+class LatexHelper:
+
+    def __init__(self, loader):
+        self.env = Environment(
+            block_start_string='\BLOCK{',
+            block_end_string='}',
+            variable_start_string='\VAR{',
+            variable_end_string='}',
+            comment_start_string='\#{',
+            comment_end_string='}',
+            line_statement_prefix='%-',
+            line_comment_prefix='%#',
+            trim_blocks=True,
+            autoescape=False,
+            loader=loader,
+            )
+
+        self.env.filters['latexify'] = self.escapelatex
+        self.env.filters['concat'] = self.concat
+        self.env.filters['max'] = self.max_len
+        self.env.filters['date'] = self.datetimeformat
+
+    def escapelatex(self, string):
+        replacements = {
+            '<': '$<$',
+            '>': '$>$',
+            '%': '\%',
+            '"': "''",
+            }
+        for (k, v) in replacements.iteritems():
+            string = string.replace(k, v)
+        string = '\\\\ \n'.join(string.splitlines())
+        return string
+
+    def concat(
+        self,
+        collection,
+        divider=',',
+        member='name',
+        ):
+        return divider.join([getattr(c, member) for c in collection])
+
+    def max_len(self, list1, list2):
+        if len(list1) > len(list2):
+            return len(list1)
+        return len(list2)
+
+    def datetimeformat(self, value, format='%d.%m.%Y'):
+        return value.strftime(format)
