1aabe071c5e2d77f0230bfe65f88603042184a4f
Jan Oliver Oelerich <janoliver@oelerich.org>
2014-04-29 14:48:57 +0200

    Update urls.py

--- fstools/urls.py
+++ fstools/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
